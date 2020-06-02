07f8c6959db8b312a8fe60915554381f4a7380bf
Sayed Yoseph Sadat <42789651+sysadat@users.noreply.github.com>
2020-06-02 01:59:42 -0700

    Add files via upload

--- region_queries.m
+++ region_queries.m
@@ -9,17 +9,17 @@ framesdir = './frames/';
 addpath('./provided_code/');
 load('kMeans.mat');
 
-% Select 3 frames 
-frames = [212, 162, 400];%changed frames
+% Select 3 frames
+frames = [600, 90, 220];%changed frames
 
-% Find the histogram and the distance and then sort them by finding minimum 
+% Find the histogram and the distance and then sort them by finding minimum
 % distance
 D = [];
 fnamesLength = length(fnames);
 for i=1:fnamesLength
     fnamesIndex = fnames(i);
     fname = [siftdir '/' fnamesIndex.name];
-    load(fname, 'descriptors'); 
+    load(fname, 'descriptors');
     transposeDescriptors = descriptors';
     transposeMeans = kMeans';
     D = distSqr(transposeDescriptors, transposeMeans);
@@ -30,43 +30,42 @@ for i=1:fnamesLength
         A(k,1) = minI;
     end
     indicies = 1:75;
-    bag_words(i,:) = histcounts(A, indicies);   
+    bag_words(i,:) = histcounts(A, indicies);
 end
 
 %MY PART
 
-for j = 1:length(frames)  
+for j = 1:length(frames)
     %load query frames image
     siftName = fnames(frames(j)).name;
     t=[siftdir '/' siftName];
     load(t);
     imname = [framesdir '/' imname];
     im = imread(imname);
-    %select regions
-    oninds = selectRegion(im,positions);
-    [r,~] = size(oninds);
-    
-    %question--------how to display the region as polygons in the picture?
-    
-    % find smilarity
-    frames_bagWords = bag_words(frames(j),:); % found histogram of the frames's bag_words
-    %find oninds similarity
+    oninds = selectRegion(im, positions);
+    onidsSize = size(oninds, 1);
+    frameIndex = frames(j);
+    frames_bagWords = bag_words(frameIndex, :);
     descriptors_oninds = [];
-    for k = 1:r
+    for k = 1:onidsSize
         index = oninds(k);
-        
-        descriptors_oninds = [descriptors_oninds; descriptors(index,:)];
+        descriptors_oninds = [descriptors_oninds; descriptors(index, :)];
     end
     B = dist2(descriptors_oninds, kMeans);
-    [~,minIB] = min(B,[],2);
-    bag_words_oninds = histcounts(minIB,1:75);
-    oninds_bagWord_matrix = repmat(bag_words_oninds', 1, length(bag_words));
-    sim = corr(bag_words',oninds_bagWord_matrix);
-    sim(isnan(sim)) = 0;
-    [sortedDis,simIm] = sort(sim,'descend');
+    [~, minIB] = min(B, [], 2);
+    indicies = 1:75;
+    bag_words_oninds = histcounts(minIB, indicies);
+    bag_wordsLength = length(bag_words);
+    transposeBagWordsOninds = bag_words_oninds';
+    oninds_bagWord_matrix = repmat(transposeBagWordsOninds, 1, bag_wordsLength);
+    bag_wordsTranspose = bag_words';
+    sim = corr(bag_wordsTranspose, oninds_bagWord_matrix);
+    isnanCheck = isnan(sim);
+    sim(isnanCheck) = 0;
+    [sortedDis, simIm] = sort(sim, 'descend');
 
     % We want to display a certain amount of similar frames. In this instance,
-    % we want to do the 5 most similar frames. 
+    % we want to do the 5 most similar frames.
     startIndex = 2;
     endIndex = 6;
     for i = startIndex:endIndex
@@ -75,7 +74,7 @@ for j = 1:length(frames)
        load(fname);
        imname = [framesdir '/' imname];
        image = imread(imname);
-       subplot(2,3,i);    
+       subplot(2,3,i);
        imshow(image);
        currentIndex = i - 1;
        stringCurrentIndex = num2str(currentIndex);
