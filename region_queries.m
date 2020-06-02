277ee5de21d509bb1b64813e8122f42a5d284703
abdullah-mohammed <40609460+abdullah-mohammed@users.noreply.github.com>
2020-06-02 03:03:59 -0700

    Add files via upload

--- region_queries.m
+++ region_queries.m
@@ -3,83 +3,81 @@ clear;
 clc;
 
 % Gain access to the paths with the SIFTS, frames and provided code
-siftdir = './sift/';
-fnames = dir([siftdir '/*.mat']);
-framesdir = './frames/';
+siftDir = './sift/';
+fNames = dir([siftDir '/*.mat']);
+framesDir = './frames/';
 addpath('./provided_code/');
 load('kMeans.mat');
 
 % Select 3 frames
-frames = [600, 90, 220];%changed frames
+frames = [600, 90, 220];
 
 % Find the histogram and the distance and then sort them by finding minimum
 % distance
 D = [];
-fnamesLength = length(fnames);
-for i=1:fnamesLength
-    fnamesIndex = fnames(i);
-    fname = [siftdir '/' fnamesIndex.name];
-    load(fname, 'descriptors');
+fNamesLength = length(fNames);
+for i=1:fNamesLength
+    fnamesIndex = fNames(i);
+    fName = [siftDir '/' fnamesIndex.name];
+    load(fName, 'descriptors');
     transposeDescriptors = descriptors';
     transposeMeans = kMeans';
     D = distSqr(transposeDescriptors, transposeMeans);
     sizeOfD = size(D);
     sizeOfDRows = sizeOfD(1);
-    for k = 1:sizeOfDRows
-        [~,minI] = min(D(k,:));
-        A(k,1) = minI;
+    for j = 1:sizeOfDRows
+        [~,minI] = min(D(j,:));
+        A(j,1) = minI;
     end
     indicies = 1:75;
-    bag_words(i,:) = histcounts(A, indicies);
+    bagOW(i,:) = histcounts(A, indicies);
 end
 
-%MY PART
-
-for j = 1:length(frames)
+for i = 1:length(frames)
     %load query frames image
-    siftName = fnames(frames(j)).name;
-    t=[siftdir '/' siftName];
-    load(t);
-    imname = [framesdir '/' imname];
-    im = imread(imname);
-    oninds = selectRegion(im, positions);
+    siftName = fNames(frames(i)).name;
+    fullPath=[siftDir '/' siftName];
+    load(fullPath);
+    imname = [framesDir '/' imname];
+    image = imread(imname);
+    oninds = selectRegion(image, positions);
     onidsSize = size(oninds, 1);
-    frameIndex = frames(j);
-    frames_bagWords = bag_words(frameIndex, :);
-    descriptors_oninds = [];
-    for k = 1:onidsSize
-        index = oninds(k);
-        descriptors_oninds = [descriptors_oninds; descriptors(index, :)];
+    frameIndex = frames(i);
+    frames_bagWords = bagOW(frameIndex, :);
+    onindDescriptors = [];
+    for j = 1:onidsSize
+        index = oninds(j);
+        onindDescriptors = [onindDescriptors; descriptors(index, :)];
     end
-    B = dist2(descriptors_oninds, kMeans);
-    [~, minIB] = min(B, [], 2);
+    B = dist2(onindDescriptors, kMeans);
+    [~, minBVal] = min(B, [], 2);
     indicies = 1:75;
-    bag_words_oninds = histcounts(minIB, indicies);
-    bag_wordsLength = length(bag_words);
-    transposeBagWordsOninds = bag_words_oninds';
-    oninds_bagWord_matrix = repmat(transposeBagWordsOninds, 1, bag_wordsLength);
-    bag_wordsTranspose = bag_words';
-    sim = corr(bag_wordsTranspose, oninds_bagWord_matrix);
-    isnanCheck = isnan(sim);
-    sim(isnanCheck) = 0;
-    [sortedDis, simIm] = sort(sim, 'descend');
+    bagOWOninds = histcounts(minBVal, indicies);
+    bagOWLength = length(bagOW);
+    transposeBagWordsOninds = bagOWOninds';
+    bagOWMatrixOninds = repmat(transposeBagWordsOninds, 1, bagOWLength);
+    bagOWTranspose = bagOW';
+    correlation = corr(bagOWTranspose, bagOWMatrixOninds);
+    notNumCheck = isnan(correlation);
+    correlation(notNumCheck) = 0;
+    [sDistance, simIm] = sort(correlation, 'descend');
 
     % We want to display a certain amount of similar frames. In this instance,
     % we want to do the 5 most similar frames.
     startIndex = 2;
     endIndex = 6;
-    for i = startIndex:endIndex
-       simImage = fnames(simIm(i));
-       fname = [siftdir '/' simImage.name];
-       load(fname);
-       imname = [framesdir '/' imname];
+    for k = startIndex:endIndex
+       simImage = fNames(simIm(k));
+       fName = [siftDir '/' simImage.name];
+       load(fName);
+       imname = [framesDir '/' imname];
        image = imread(imname);
-       subplot(2,3,i);
+       subplot(2,3,k);
        imshow(image);
-       currentIndex = i - 1;
+       currentIndex = k - 1;
        stringCurrentIndex = num2str(currentIndex);
        resultTitle = strcat('Result Number: ', stringCurrentIndex);
-       stringDistance = num2str(sortedDis(i));
+       stringDistance = num2str(sDistance(k));
        distanceTitle = strcat('Total Distance: ', stringDistance);
        titleName = strcat(resultTitle, imname, distanceTitle);
        title(titleName);
